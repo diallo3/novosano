82e7485e3c5d924971105a6a924fc35db8a59243
Diallo Hudgins <d@diallohudgins.com>
2017-05-17 00:08:33 -0400

    added prelimary heros

---
+++ wp-content/themes/novosano/page-home.php
@@ -0,0 +1,17 @@
+<?php 
+/*
+ * Template Name: Page Home
+ 
+ * The template for displaying the home page.
+ *
+ * @package  WordPress
+ * @subpackage  Opus
+ * @since    Opus 0.1
+ */
+$context = Timber::get_context(); 
+$post = new TimberPost(); 
+$context["post"] = $post; 
+$context["acf"] = get_field_objects($data["post"]->ID);
+Timber::render("/views/page-home.twig", $context); 
+
+?>
