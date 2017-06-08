d2598696a3ff9dc2320f3e33c965928b95a1251b
Diallo Hudgins <d@diallohudgins.com>
2017-06-08 14:43:32 -0400

    added video items to header. Blocked out scripts and added partials

---
+++ wp-content/themes/novosano/assets/js/scripts/1-main.js
@@ -0,0 +1,51 @@
+(function($) {
+
+  // Foundation init
+  $(document).foundation();
+
+  $(document).ready(function() {
+        // Remove empty P tags created by WP inside of Accordion and Orbit
+        $('.accordion p:empty, .orbit p:empty').remove();
+
+        // Makes sure last grid item floats left
+        $('.archive-grid .columns').last().addClass( 'end' );
+
+        // Adds Flex Video to YouTube and Vimeo Embeds
+        $('iframe[src*="youtube.com"], iframe[src*="vimeo.com"]').each(function() {
+            if ( $(this).innerWidth() / $(this).innerHeight() > 1.5 ) {
+                $(this).wrap("<div class='widescreen flex-video'/>");
+            } else {
+                $(this).wrap("<div class='flex-video'/>");
+            }
+        });
+
+        // CoverVid
+        if($('.c-vid').length > 0) {
+            $('.c-vid__bck').coverVid(1920, 1080);
+        }
+        
+
+
+        // Odd Even for homepage
+        function skCount() {
+            var $sk = $('.c-sidekick__block');
+
+            $sk.each(function(i){
+                if(!(i%2))  
+                    $(this).addClass('odd');
+                else
+                    $(this).addClass('even');
+                
+            });
+
+        }
+        return skCount();
+
+        
+
+
+
+  });
+
+
+})(jQuery);
