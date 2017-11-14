0a57f02eafdcfd4229128a949548fccbf5366b91
Diallo Hudgins <d@diallohudgins.com>
2017-11-14 11:27:17 -0500

    updating fields for Journey and Analytics Theme Options

--- wp-content/themes/novosano/page-journey.php
+++ wp-content/themes/novosano/page-journey.php
@@ -11,6 +11,7 @@
 $context = Timber::get_context(); 
 $post = new TimberPost(); 
 $context["post"] = $post; 
+$context["acf"] = get_field_objects($data["post"]->ID);
 Timber::render("/views/pages/page-journey.twig", $context); 
 
 ?>
