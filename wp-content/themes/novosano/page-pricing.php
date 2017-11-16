d90a6196e3d988bdbd5d118d7418cde85b0edcc9
Diallo Hudgins <d@diallohudgins.com>
2017-11-16 14:22:09 -0500

    Added Pricing page template and ACF Fields. Moved homepage CTAs into theme options

---
+++ wp-content/themes/novosano/page-pricing.php
@@ -0,0 +1,19 @@
+<?php 
+/*
+ * Template Name: Page Pricing
+ 
+ * The template for displaying the home page.
+ *
+ * @package  WordPress
+ * @subpackage  Opus
+ * @since    Opus 0.1
+ */
+$context = Timber::get_context(); 
+$post = new TimberPost(); 
+$context["post"] = $post; 
+/* post types */
+$context['testimonial'] = Timber::get_posts( $testi_args );
+$context["acf"] = get_field_objects($data["post"]->ID);
+Timber::render("/views/pages/page-pricing.twig", $context); 
+
+?>
